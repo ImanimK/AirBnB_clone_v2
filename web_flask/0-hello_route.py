db03eb17a48ea8529221b76b6a15f9d1981e05ce
root <root@Imani>
2024-04-20 10:09:27 +0300

    webframe

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,13 @@
 Your web application must be listening on 0.0.0.0, port 5000
 """
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def hello_route():
-    return 'Hello HBNB!'.strip()
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
